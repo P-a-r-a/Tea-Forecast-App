23941c59fdec06db513a3dd2302836b8c27dffaa
P-a-r-a <paramiathukorala@gmail.com>
2026-07-03 11:25:12 +0530

    bug fix

--- app.py
+++ app.py
@@ -565,6 +565,7 @@ with st.sidebar:
 
             .load {
                 position: absolute;
+                width: 100%;
                 bottom: 0;
                 left: 50%;
                 transform: translateX(-50%);
